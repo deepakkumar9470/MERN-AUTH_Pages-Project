4f633a95342e6608e2faefbdf82d7dc6d3075039
deepakkumar9470 <dk397787@gmail.com>
2024-02-22 11:05:33 +0530

    render to cyclic changed

--- frontend/src/redux/usersApiSlice.js
+++ frontend/src/redux/usersApiSlice.js
@@ -1,33 +1,33 @@
 import { apiSlice } from './apiSlice';
 
-const URL = 'http://localhost:5000';
-// const DEPLOYED_URL = 'https://mern-users-list-backend.onrender.com';
+// const URL = 'http://localhost:5000';
+const DEPLOYED_URL = 'https://frightened-colt-hat.cyclic.app';
 
 export const userApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (data) => ({
-                url: `${URL}/api/user/login`,
+                url: `${DEPLOYED_URL}/api/user/login`,
                 method: 'POST',
                 body: data
             }),
         }),
         logout: builder.mutation({
             query: (data) => ({
-                url: `${URL}/api/user/logout`,
+                url: `${DEPLOYED_URL}/api/user/logout`,
                 method: 'POST',
             }),
         }),
         register: builder.mutation({
             query: (data) => ({
-                url: `${URL}/api/user/register`,
+                url: `${DEPLOYED_URL}/api/user/register`,
                 method: 'POST',
                 body: data
             }),
         }),
         getSingleUserById: builder.mutation({
             query: (id) => ({
-                url: `${URL}/api/user/${id}`,
+                url: `${DEPLOYED_URL}/api/user/${id}`,
                 method: 'GET',
             }),
         }),
--- frontend/src/services/helper.js
+++ frontend/src/services/helper.js
@@ -1,3 +1,3 @@
-export const BASE_URL = `http://localhost:5000`
-// export const BASE_URL = `https://mern-users-list-backend.onrender.com`
+// export const BASE_URL = `http://localhost:5000`
+export const BASE_URL = `https://frightened-colt-hat.cyclic.app`
 
