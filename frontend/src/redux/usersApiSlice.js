57547c99ebd94a8e404d131fe5e5795f95b877ea
deepakkumar9470 <dk397787@gmail.com>
2024-02-22 10:37:16 +0530

    changed mongo url

--- frontend/src/redux/usersApiSlice.js
+++ frontend/src/redux/usersApiSlice.js
@@ -1,33 +1,33 @@
 import { apiSlice } from './apiSlice';
 
-// const URL = 'http://localhost:5000';
-const DEPLOYED_URL = 'https://mern-users-list-backend.onrender.com';
+const URL = 'http://localhost:5000';
+// const DEPLOYED_URL = 'https://mern-users-list-backend.onrender.com';
 
 export const userApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (data) => ({
-                url: `${DEPLOYED_URL}/api/user/login`,
+                url: `${URL}/api/user/login`,
                 method: 'POST',
                 body: data
             }),
         }),
         logout: builder.mutation({
             query: (data) => ({
-                url: `${DEPLOYED_URL}/api/user/logout`,
+                url: `${URL}/api/user/logout`,
                 method: 'POST',
             }),
         }),
         register: builder.mutation({
             query: (data) => ({
-                url: `${DEPLOYED_URL}/api/user/register`,
+                url: `${URL}/api/user/register`,
                 method: 'POST',
                 body: data
             }),
         }),
         getSingleUserById: builder.mutation({
             query: (id) => ({
-                url: `${DEPLOYED_URL}/api/user/${id}`,
+                url: `${URL}/api/user/${id}`,
                 method: 'GET',
             }),
         }),
